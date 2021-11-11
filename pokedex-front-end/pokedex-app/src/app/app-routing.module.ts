3943204b147f1dc0a29d884ce8ae8b5e0d06c252
Katarzyna Wasik <kat.wasik@hotmail.com>
2021-11-11 18:10:37 +0100

    Add header

--- pokedex-front-end/pokedex-app/src/app/app-routing.module.ts
+++ pokedex-front-end/pokedex-app/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
+import { TrainerListComponent } from './components/trainer-list/trainer-list.component';
+import { TeamListComponent } from './components/team-list/team-list.component';
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PokemonListComponent,
+  },
+  {
+    path: 'team',
+    component: TeamListComponent,
+  },
+  {
+    path: 'pokedex',
+    component: PokemonListComponent,
+  },
+  {
+    path: 'trainer',
+    component: TrainerListComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
