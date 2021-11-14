afabc222068a40a00560b9d443b90547bd6baa40
vivi-chaudiem <nguyen.chaudiem@yahoo.de>
2021-11-14 21:47:49 +0100

    Trainer service complete including tests

--- pokedex-front-end/pokedex-app/src/app/services/trainer.service.ts
+++ pokedex-front-end/pokedex-app/src/app/services/trainer.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TrainerService {
 
-  constructor() { }
+  readonly baseURL:string = "http://localhost:4200/api/v1/trainers"; // add the readonly attribute
+
+  constructor(
+    private http:HttpClient
+  ) { }
+
+  getTrainer(): Observable<any> {
+    return this.http.get<any>(this.baseURL + '/');
+   }
 }
