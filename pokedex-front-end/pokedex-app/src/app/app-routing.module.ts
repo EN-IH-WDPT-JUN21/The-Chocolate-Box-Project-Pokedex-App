4f0c1ae81dd85cd8bd96ffdf8672c320ae8750fb
Nathan Giles <gills11@gmail.com>
2021-11-15 09:55:18 +0000

    Updated Team List Components

--- pokedex-front-end/pokedex-app/src/app/app-routing.module.ts
+++ pokedex-front-end/pokedex-app/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { TeamListComponent } from './components/team-list/team-list.component';
+import { TeamDetailComponent } from './components/team-detail/team-detail.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'team/:teamId',
+    component: TeamDetailComponent
+  },
+  {
+    path: 'teams',
+    component: TeamListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- pokedex-front-end/pokedex-app/src/app/components/team-detail/team-detail.component.ts
+++ pokedex-front-end/pokedex-app/src/app/components/team-detail/team-detail.component.ts
@@ -1,15 +1,33 @@
+import { TeamService } from './../../services/team.service';
 import { Component, OnInit } from '@angular/core';
+import { TeamDTO } from 'src/app/models/team-model';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-team-detail',
+  selector: 'team-detail',
   templateUrl: './team-detail.component.html',
   styleUrls: ['./team-detail.component.css']
 })
 export class TeamDetailComponent implements OnInit {
 
-  constructor() { }
+  team!:TeamDTO;
+  result:any;
+
+  constructor(private teamService:TeamService,
+    private router:Router,
+    private activatedRoute:ActivatedRoute) { }
 
   ngOnInit(): void {
+    const teamId:number = this.activatedRoute.snapshot.params['teamId'];
+    this.showTeam(teamId);
   }
+
+  showTeam(id:number){
+    this.teamService.getTeamById(id).subscribe(result => 
+      this.team = result
+    )
+  };
+
+
 
 }
